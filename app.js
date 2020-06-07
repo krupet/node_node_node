871947c2558544b30f286a74081b06c1fd709c79
Petro Krutsiuk <p.krutsiuk@unicheck.com>
2020-06-07 13:57:50 +0300

    add frontend

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
+const path = require('path')
 
 const app = express();
 const port = 8000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const viewsDir = path.join(__dirname, 'views')
+app.use(express.static(viewsDir))
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './weights')))
+app.use(express.static(path.join(__dirname, './dist')))
 
 app.listen(port, () => {
     console.log('listen port 8000');
@@ -16,8 +26,11 @@ MongoClient.connect('mongodb://localhost:27017/data', {useUnifiedTopology: true}
         const db = client.db('data')
         const quotesCollection = db.collection('expressions')
 
+        app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
+
         app.post('/data', (req, res) => {
             quotesCollection.insertOne(req.body)
+                .then(result => { console.log("saved") })
                 .catch(error => console.error(error))
         })
     })
