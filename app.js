57e2087f0e85e4db4c656d3857e10d05e19115f1
Petro Krutsiuk <p.krutsiuk@unicheck.com>
2020-06-08 21:53:06 +0300

    Add line chart

--- app.js
+++ app.js
@@ -30,21 +30,25 @@ MongoClient.connect('mongodb://localhost:27017/data', {useUnifiedTopology: true}
         const quotesCollection = db.collection('expressions')
 
         app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
+        app.get('/sendMeToTheAnalytics', (req, res) => res.redirect('/analytics'))
 
         app.post('/data', (req, res) => {
             const payload = JSON.parse(req.body.data)
             payload._id = new ObjectID()
             quotesCollection.insertOne(payload)
-                .then(result => {
-                    console.log("saved")
-                })
+                // .then(result => {
+                //     console.log("saved")
+                // })
                 .catch(error => console.error(error))
         })
 
+        const numberOfSamples = 16; // number of time samples used for chart generation
         app.get('/analytics', (req, res) => {
-            db.collection('expressions').find().toArray()
+            db.collection('expressions').find({"expressions": {$exists: true, $ne: null}})
+                .sort({ts: -1}).limit(numberOfSamples).toArray()
                 .then(results => {
-                    const chartData = {
+
+                    const pieData = {
                         neutral: 0.0,
                         happy: 0.0,
                         sad: 0.0,
@@ -53,54 +57,56 @@ MongoClient.connect('mongodb://localhost:27017/data', {useUnifiedTopology: true}
                         disgusted: 0.0,
                         surprised: 0.0
                     }
+
+                    const lineData = {
+                        neutral: [],
+                        happy: [],
+                        sad: [],
+                        angry: [],
+                        fearful: [],
+                        disgusted: [],
+                        surprised: [],
+                        labels: []
+                    }
                     const expressions = results.map(a => a.expressions)
 
                     for (let i = 0; i < expressions.length; i++) {
 
                         const expression = expressions[i]
 
-                        chartData.neutral = (parseFloat(chartData.neutral) + parseFloat(expression.neutral)).toFixed(3)
-                        chartData.happy = (parseFloat(chartData.happy) + parseFloat(expression.happy)).toFixed(3)
-                        chartData.sad = (parseFloat(chartData.sad) + parseFloat(expression.sad)).toFixed(3)
-                        chartData.angry = (parseFloat(chartData.angry) + parseFloat(expression.angry)).toFixed(3)
-                        chartData.fearful = (parseFloat(chartData.fearful) + parseFloat(expression.fearful)).toFixed(3)
-                        chartData.disgusted = (parseFloat(chartData.disgusted) + parseFloat(expression.disgusted)).toFixed(3)
-                        chartData.surprised = (parseFloat(chartData.surprised) + parseFloat(expression.surprised)).toFixed(3)
+                        pieData.neutral = (parseFloat(pieData.neutral) + parseFloat(expression.neutral)).toFixed(3)
+                        pieData.happy = (parseFloat(pieData.happy) + parseFloat(expression.happy)).toFixed(3)
+                        pieData.sad = (parseFloat(pieData.sad) + parseFloat(expression.sad)).toFixed(3)
+                        pieData.angry = (parseFloat(pieData.angry) + parseFloat(expression.angry)).toFixed(3)
+                        pieData.fearful = (parseFloat(pieData.fearful) + parseFloat(expression.fearful)).toFixed(3)
+                        pieData.disgusted = (parseFloat(pieData.disgusted) + parseFloat(expression.disgusted)).toFixed(3)
+                        pieData.surprised = (parseFloat(pieData.surprised) + parseFloat(expression.surprised)).toFixed(3)
                     }
 
-                    res.render('analitics.ejs', {pieData: chartData})
+                    // elemets go in DESCENDING order and we need ASCENDING order on the line chart
+                    const reversed = results.reverse()
+                    for (let i = 0; i < reversed.length; i++) {
+
+                        let element = reversed[i];
+                        const expression = element.expressions
+
+                        lineData.neutral.push(parseFloat(expression.neutral).toFixed(3))
+                        lineData.happy.push(parseFloat(expression.happy).toFixed(3))
+                        lineData.sad.push(parseFloat(expression.sad).toFixed(3))
+                        lineData.angry.push(parseFloat(expression.angry).toFixed(3))
+                        lineData.fearful.push(parseFloat(expression.fearful).toFixed(3))
+                        lineData.disgusted.push(parseFloat(expression.disgusted).toFixed(3))
+                        lineData.surprised.push(parseFloat(expression.surprised).toFixed(3))
+
+                        const date = new Date(element.ts)
+                        lineData.labels.push(date.toISOString().replace(/T/, ' ').replace(/\..+/, ''))
+                    }
+
+                    res.render('analitics.ejs', {
+                        pieData: pieData,
+                        lineData: lineData
+                    })
                 })
                 .catch(error => console.error(error))
-        })
-
-        app.get('/pieData', (req, res) => {
-            db.collection('expressions').find().toArray()
-                .then(results => {
-                    const chartData = {
-                        neutral: 0.0,
-                        happy: 0.0,
-                        sad: 0.0,
-                        angry: 0.0,
-                        fearful: 0.0,
-                        disgusted: 0.0,
-                        surprised: 0.0
-                    }
-                    const expressions = results.map(a => a.expressions)
-
-                    for (let i = 0; i < expressions.length; i++) {
-
-                        const expression = expressions[i]
-
-                        chartData.neutral = (parseFloat(chartData.neutral) + parseFloat(expression.neutral)).toFixed(3)
-                        chartData.happy = (parseFloat(chartData.happy) + parseFloat(expression.happy)).toFixed(3)
-                        chartData.sad = (parseFloat(chartData.sad) + parseFloat(expression.sad)).toFixed(3)
-                        chartData.angry = (parseFloat(chartData.angry) + parseFloat(expression.angry)).toFixed(3)
-                        chartData.fearful = (parseFloat(chartData.fearful) + parseFloat(expression.fearful)).toFixed(3)
-                        chartData.disgusted = (parseFloat(chartData.disgusted) + parseFloat(expression.disgusted)).toFixed(3)
-                        chartData.surprised = (parseFloat(chartData.surprised) + parseFloat(expression.surprised)).toFixed(3)
-                    }
-
-                    res.send(JSON.stringify(chartData))
-                }).catch(error => console.error(error))
         })
     })
